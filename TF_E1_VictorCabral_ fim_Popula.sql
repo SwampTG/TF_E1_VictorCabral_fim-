4e262ed620214959e025efaf173d7b2cd024548c
adrian_s_lps <adriansoareslps@gmail.com>
2023-02-12 17:26:46 -0300

    Finished popula scripts

--- TF_E1_VictorCabral_ fim_Popula.sql
+++ TF_E1_VictorCabral_ fim_Popula.sql
@@ -18,7 +18,7 @@ CREATE DATABASE IF NOT EXISTS TF_TemaE_VictorCabral;
 USE TF_TemaE_VictorCabral;
 
 -- POPULA
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
         INTO TABLE ESTABELECIMENTO
     FIELDS TERMINATED BY ';'
     ENCLOSED BY '"'
@@ -64,7 +64,7 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00
     estabelecimento_municipio_nome=@estabelecimento_municipio_nome,
     estabelecimento_uf=@estabelecimento_uf;
 
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
 	INTO TABLE SISTEMA
     FIELDS TERMINATED BY ';'
     ENCLOSED BY '"'
@@ -105,7 +105,7 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00
 		@sistema_origem
 	) SET sistema_origem=@sistema_origem;
     
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
 	INTO TABLE PACIENTE
 	FIELDS TERMINATED BY ';'
 	ENCLOSED BY '"'
@@ -152,7 +152,7 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00
 		paciente_nacionalidade_enumNacionalidade=@paciente_nacionalidade_enumNacionalidade,
 		paciente_enumSexoBioIogico=@paciente_enumSexoBioIogico;
     
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
 		INTO TABLE endereco
 	FIELDS TERMINATED BY ';'
 	ENCLOSED BY '"'
@@ -199,7 +199,7 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00
 		paciente_endereco_coPais=@paciente_endereco_coPais, 
 		paciente_endereco_coIbgeMunicipio=@paciente_endereco_coIbgeMunicipio;
         
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/VACINA.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/VACINA.csv'
 		INTO TABLE VACINA
 	FIELDS TERMINATED BY ','
 	ENCLOSED BY '"'
@@ -212,7 +212,7 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/VACINA.
         @vacina_fabricante_referencia
 	) SET vacina_codigo=@vacina_codigo, vacina_nome=@vacina_nome, vacina_fabricante_referencia=@vacina_fabricante_referencia;
 
-LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/SISTEMA_CADASTRO.csv'
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/SISTEMA_CADASTRO.csv'
 		INTO TABLE SISTEMA_CADASTRO
 	FIELDS TERMINATED BY ','
 	ENCLOSED BY '"'
@@ -223,3 +223,43 @@ LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/SISTEMA
 		@vacina_codigo,
 		@sistema
 	) SET vacina=@vacina_codigo, sistema=@sistema;
+    
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/ARMAZENADA_EST.csv'
+		INTO TABLE ARMAZENADA_ESTABELECIMENTO
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@dummy,
+		@estabelecimento_id,
+		@idCadastro,
+        @vacina_lote
+	) SET estabelecimento=@estabelecimento_id, 
+		cadastro=@idCadastro,
+        vacina_lote=@vacina_lote;
+
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/TF_E1_VictorCabral_fim/raw_data/recebe.csv'
+		INTO TABLE recebe
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@dummy,
+		@paciente_id,
+		@armazenada,
+        @vacina_dataAplicacao,
+        @vacina_grupoAtendimento_codigo,
+        @vacina_grupoAtendimento_nome,
+        @vacina_descricao_dose,
+        @vacina_categoria_nome,
+        @vacina_categoria_codigo
+	) SET paciente=@paciente_id,
+		armazenada=@armazenada,
+        vacina_dataAplicacao=@vacina_dataAplicacao,
+        vacina_grupo_atendimento_code=@vacina_grupoAtendimento_codigo,
+        vacina_grupo_atendimento_nome=@vacina_grupoAtendimento_nome,
+        vacina_descricao_dose=@vacina_descricao_dose,
+        vacina_categoria_nome=@vacina_categoria_nome,
+        vacina_categoria_codigo=@vacina_categoria_codigo;
