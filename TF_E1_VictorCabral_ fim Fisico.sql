ad3c33875acc550bc9cc6a4c74728c10e8ed3de4
adrian_s_lps <adriansoareslps@gmail.com>
2023-02-11 21:26:56 -0300

    CSV data extraction

--- TF_E1_VictorCabral_ fim Fisico.sql
+++ TF_E1_VictorCabral_ fim Fisico.sql
@@ -22,44 +22,31 @@ CREATE TABLE ESTABELECIMENTO (
     estabelecimento_id BIGINT NOT NULL AUTO_INCREMENT, 
     estabelecimento_valor INT NOT NULL,
     estabelecimento_razaoSocial VARCHAR(50) NOT NULL,
-    estalecimento_noFantasia INT(6) NOT NULL,
+    estalecimento_noFantasia DECIMAL(6, 0) NOT NULL,
     estabelecimento_municipio_nome VARCHAR(50) NOT NULL,
     estabelecimento_uf VARCHAR(2) NOT NULL,
-
+	UNIQUE(estabelecimento_valor, estabelecimento_razaoSocial),
     CONSTRAINT ESTABELECIMENTO_PK PRIMARY KEY (estabelecimento_id)   
 ) ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE SISTEMA (
     sistema_id INT NOT NULL AUTO_INCREMENT, 
     sistema_origem VARCHAR(30) NOT NULL,
-
+    UNIQUE (sistema_origem),
     CONSTRAINT SISTEMA_PK PRIMARY KEY (sistema_id)   
 ) ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE VACINA (
-    vacina_codigo INT NOT NULL, 
-    vacina_lote VARCHAR(8) NOT NULL,
-    vacina_descricao_dose VARCHAR(10) NOT NULL,
-    vacina_nome VARCHAR(30) NOT NULL,
-    vacina_fabricante_referencia VARCHAR(30) NOT NULL,
-    vacina_categoria_nome VARCHAR(15) NOT NULL,
-    vacina_categoria_codigo INT NOT NULL,
-    vacina_grupo_atendimento_code INT NOT NULL,
-    vacina_grupo_atendimento_nome VARCHAR(80) NOT NULL,
-    estabelecimento_id BIGINT NOT NULL,
-    sistema_id INT NOT NULL,
-
+	vacina_codigo INT NOT NULL,
+	vacina_nome VARCHAR(30) NOT NULL,
+    vacina_fabricante_referencia VARCHAR(30),
+    UNIQUE (vacina_codigo, vacina_nome),
     CONSTRAINT VACINA_PK PRIMARY KEY (vacina_codigo),
-    
-    CONSTRAINT VACINA_ESTABELECIMENTO_FK 
-		FOREIGN KEY (estabelecimento_id)
-        REFERENCES ESTABELECIMENTO (estabelecimento_id)
-        ON DELETE RESTRICT,
-    CONSTRAINT VACINA_SISTEMA_FK 
-		FOREIGN KEY (sistema_id)
-        REFERENCES SISTEMA (sistema_id)
+    CONSTRAINT UNIDADE_VACINA_FK 
+		FOREIGN KEY (vacina_codigo)
+        REFERENCES VACINA (vacina_codigo)
         ON DELETE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
+);
 
 CREATE TABLE PACIENTE (
     paciente_id VARCHAR(100) NOT NULL, 
@@ -69,19 +56,17 @@ CREATE TABLE PACIENTE (
     paciente_racaCor_codigo ENUM('1', '2', '3', '4', '99') NOT NULL,
     paciente_nacionalidade_enumNacionalidade ENUM('B', 'E', 'N', 'None'),
     paciente_enumSexoBioIogico ENUM('M', 'F') NOT NULL,
-
     CONSTRAINT PACIENTE_PK PRIMARY KEY (paciente_id)
-) ENGINE = InnoDB;
+) ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE endereco (
 	paciente_id VARCHAR(100) NOT NULL, 
-    paciente_endereco_cep INT(8) NOT NULL, 
+    paciente_endereco_cep DECIMAL(8,0) NOT NULL, 
     paciente_endereco_uf VARCHAR(2) NOT NULL,
     paciente_endereco_nmPais VARCHAR(30) NOT NULL,
     paciente_endereco_nmMunicipio VARCHAR(30) NOT NULL,
     paciente_endereco_coPais INT NOT NULL, 
     paciente_endereco_coIbgeMunicipio INT NOT NULL,     
-
     CONSTRAINT endereco_UK UNIQUE KEY (paciente_endereco_cep, paciente_id),
     CONSTRAINT endereco_paciente_FK 
 		FOREIGN KEY (paciente_id)
@@ -89,42 +74,52 @@ CREATE TABLE endereco (
         ON DELETE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1;
 
-CREATE TABLE recebe (
-	paciente VARCHAR(100) NOT NULL,
+CREATE TABLE SISTEMA_CADASTRO (
+	idCadastro INT NOT NULL AUTO_INCREMENT,
+	sistema INT NOT NULL,
     vacina INT NOT NULL,
-    vacina_dataAplicacao DATE NOT NULL,
-    CONSTRAINT recebeu_PACIENTE_FK 
-		FOREIGN KEY (paciente)
-        REFERENCES PACIENTE (paciente_id)
+    CONSTRAINT SISTEMA_CADASTRO_PK PRIMARY KEY (idCadastro),
+    CONSTRAINT SISTEMA_CADASTRO_SISTEMA_FK 
+		FOREIGN KEY (sistema)
+        REFERENCES SISTEMA (sistema_id)
         ON DELETE RESTRICT,
-	CONSTRAINT recebeu_VACINA_FK
+	CONSTRAINT SISTEMA_CADASTRO_VACINA_FK
 		FOREIGN KEY (vacina)
         REFERENCES VACINA (vacina_codigo)
         ON DELETE RESTRICT
 );
 
-CREATE TABLE armazena (
+CREATE TABLE ARMAZENADA_ESTABELECIMENTO (
+	idArmazenamento INT NOT NULL AUTO_INCREMENT,
 	estabelecimento BIGINT NOT NULL,
-    vacina INT NOT NULL,
+    cadastro INT NOT NULL,
+    vacina_lote VARCHAR(8) NOT NULL,
+    CONSTRAINT ARMAZENADA_PK PRIMARY KEY (idArmazenamento),
     CONSTRAINT armazena_ESTABELECIMENTO_FK 
 		FOREIGN KEY (estabelecimento)
         REFERENCES ESTABELECIMENTO (estabelecimento_id)
         ON DELETE RESTRICT,
-	CONSTRAINT armazena_VACINA_FK
-		FOREIGN KEY (vacina)
-        REFERENCES VACINA (vacina_codigo)
+	CONSTRAINT armazena_CADASTRO_FK
+		FOREIGN KEY (cadastro)
+        REFERENCES SISTEMA_CADASTRO (idCadastro)
         ON DELETE RESTRICT
 );
 
-CREATE TABLE vincula (
-	sistema INT NOT NULL,
-    vacina INT NOT NULL,
-    CONSTRAINT vincula_SISTEMA_FK 
-		FOREIGN KEY (sistema)
-        REFERENCES SISTEMA (sistema_id)
+CREATE TABLE recebe (
+	paciente VARCHAR(100) NOT NULL,
+    armazenada INT NOT NULL,
+    vacina_dataAplicacao DATE NOT NULL,
+    vacina_grupo_atendimento_code INT NOT NULL,
+    vacina_grupo_atendimento_nome VARCHAR(80) NOT NULL,
+    vacina_descricao_dose VARCHAR(10) NOT NULL,
+    vacina_categoria_nome VARCHAR(15) NOT NULL,
+    vacina_categoria_codigo INT NOT NULL,
+    CONSTRAINT recebeu_PACIENTE_FK 
+		FOREIGN KEY (paciente)
+        REFERENCES PACIENTE (paciente_id)
         ON DELETE RESTRICT,
-	CONSTRAINT vincula_VACINA_FK
-		FOREIGN KEY (vacina)
-        REFERENCES VACINA (vacina_codigo)
+	CONSTRAINT recebeu_ARMAZENADA_FK
+		FOREIGN KEY (armazenada)
+        REFERENCES ARMAZENADA_ESTABELECIMENTO (idArmazenamento)
         ON DELETE RESTRICT
 );
---
+++ TF_E1_VictorCabral_ fim_Popula.sql
@@ -0,0 +1,225 @@
+-- ------------------------   << Trabalho Final (TF) - Tema E - Data Lake  >>   ------------------------
+--
+--                    					SCRIPT DE CRIACAO (DDL) 
+--
+-- Data Criacao ...........: 09/02/2023
+-- Autor(es) ..............: Adrian Soares Lopes, Pedro Lucas Cassiano Martins e Victor de Souza Cabral
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: TF_TemaE_VictorCabral
+--
+-- PROJETO => 01 Base de Dados
+--         => 05 Tabelas
+--
+-- ----------------------------------------------------------------------------------------------------
+
+-- BASE DE DADOS
+CREATE DATABASE IF NOT EXISTS TF_TemaE_VictorCabral;
+
+USE TF_TemaE_VictorCabral;
+
+-- POPULA
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+        INTO TABLE ESTABELECIMENTO
+    FIELDS TERMINATED BY ';'
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (
+		@document_id,
+		@paciente_id,
+		@paciente_idade,
+		@paciente_dataNascimento,
+		@paciente_enumSexoBiologico,
+		@paciente_racaCor_codigo,
+		@paciente_racaCor_valor,
+		@paciente_endereco_coIbgeMunicipio,
+		@paciente_endereco_coPais,
+		@paciente_endereco_nmMunicipio,
+		@paciente_endereco_nmPais,
+		@paciente_endereco_uf,
+		@paciente_endereco_cep,
+		@paciente_nacionalidade_enumNacionalidade,
+		@estabelecimento_valor,
+		@estabelecimento_razaoSocial,
+		@estalecimento_noFantasia,
+		@estabelecimento_municipio_codigo,
+		@estabelecimento_municipio_nome,
+		@estabelecimento_uf,
+		@vacina_grupoAtendimento_codigo,
+		@vacina_grupoAtendimento_nome,
+		@vacina_categoria_codigo,
+		@vacina_categoria_nome,
+		@vacina_lote,
+		@vacina_fabricante_nome,
+		@vacina_fabricante_referencia,
+		@vacina_dataAplicacao,
+		@vacina_descricao_dose,
+		@vacina_codigo,
+		@vacina_nome,
+		@sistema_origem
+	)
+    SET estabelecimento_valor=@estabelecimento_valor,
+    estabelecimento_razaoSocial=@estabelecimento_razaoSocial,
+    estalecimento_noFantasia=@estalecimento_noFantasia,
+    estabelecimento_municipio_nome=@estabelecimento_municipio_nome,
+    estabelecimento_uf=@estabelecimento_uf;
+
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+	INTO TABLE SISTEMA
+    FIELDS TERMINATED BY ';'
+    ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+    (
+		@document_id,
+		@paciente_id,
+		@paciente_idade,
+		@paciente_dataNascimento,
+		@paciente_enumSexoBiologico,
+		@paciente_racaCor_codigo,
+		@paciente_racaCor_valor,
+		@paciente_endereco_coIbgeMunicipio,
+		@paciente_endereco_coPais,
+		@paciente_endereco_nmMunicipio,
+		@paciente_endereco_nmPais,
+		@paciente_endereco_uf,
+		@paciente_endereco_cep,
+		@paciente_nacionalidade_enumNacionalidade,
+		@estabelecimento_valor,
+		@estabelecimento_razaoSocial,
+		@estalecimento_noFantasia,
+		@estabelecimento_municipio_codigo,
+		@estabelecimento_municipio_nome,
+		@estabelecimento_uf,
+		@vacina_grupoAtendimento_codigo,
+		@vacina_grupoAtendimento_nome,
+		@vacina_categoria_codigo,
+		@vacina_categoria_nome,
+		@vacina_lote,
+		@vacina_fabricante_nome,
+		@vacina_fabricante_referencia,
+		@vacina_dataAplicacao,
+		@vacina_descricao_dose,
+		@vacina_codigo,
+		@vacina_nome,
+		@sistema_origem
+	) SET sistema_origem=@sistema_origem;
+    
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+	INTO TABLE PACIENTE
+	FIELDS TERMINATED BY ';'
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@document_id,
+		@paciente_id,
+		@paciente_idade,
+		@paciente_dataNascimento,
+		@paciente_enumSexoBiologico,
+		@paciente_racaCor_codigo,
+		@paciente_racaCor_valor,
+		@paciente_endereco_coIbgeMunicipio,
+		@paciente_endereco_coPais,
+		@paciente_endereco_nmMunicipio,
+		@paciente_endereco_nmPais,
+		@paciente_endereco_uf,
+		@paciente_endereco_cep,
+		@paciente_nacionalidade_enumNacionalidade,
+		@estabelecimento_valor,
+		@estabelecimento_razaoSocial,
+		@estalecimento_noFantasia,
+		@estabelecimento_municipio_codigo,
+		@estabelecimento_municipio_nome,
+		@estabelecimento_uf,
+		@vacina_grupoAtendimento_codigo,
+		@vacina_grupoAtendimento_nome,
+		@vacina_categoria_codigo,
+		@vacina_categoria_nome,
+		@vacina_lote,
+		@vacina_fabricante_nome,
+		@vacina_fabricante_referencia,
+		@vacina_dataAplicacao,
+		@vacina_descricao_dose,
+		@vacina_codigo,
+		@vacina_nome,
+		@sistema_origem
+	) SET paciente_id=@paciente_id, 
+		paciente_dataNascimento=@paciente_dataNascimento,
+		paciente_idade=@paciente_idade,
+		paciente_racaCor_valor=@paciente_racaCor_valor,
+		paciente_racaCor_codigo=@paciente_racaCor_valor,
+		paciente_nacionalidade_enumNacionalidade=@paciente_nacionalidade_enumNacionalidade,
+		paciente_enumSexoBioIogico=@paciente_enumSexoBioIogico;
+    
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/part-00000-b7e405c1-20aa-4d8a-8f05-38b4f023f129.c000.csv'
+		INTO TABLE endereco
+	FIELDS TERMINATED BY ';'
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@document_id,
+		@paciente_id,
+		@paciente_idade,
+		@paciente_dataNascimento,
+		@paciente_enumSexoBiologico,
+		@paciente_racaCor_codigo,
+		@paciente_racaCor_valor,
+		@paciente_endereco_coIbgeMunicipio,
+		@paciente_endereco_coPais,
+		@paciente_endereco_nmMunicipio,
+		@paciente_endereco_nmPais,
+		@paciente_endereco_uf,
+		@paciente_endereco_cep,
+		@paciente_nacionalidade_enumNacionalidade,
+		@estabelecimento_valor,
+		@estabelecimento_razaoSocial,
+		@estalecimento_noFantasia,
+		@estabelecimento_municipio_codigo,
+		@estabelecimento_municipio_nome,
+		@estabelecimento_uf,
+		@vacina_grupoAtendimento_codigo,
+		@vacina_grupoAtendimento_nome,
+		@vacina_categoria_codigo,
+		@vacina_categoria_nome,
+		@vacina_lote,
+		@vacina_fabricante_nome,
+		@vacina_fabricante_referencia,
+		@vacina_dataAplicacao,
+		@vacina_descricao_dose,
+		@vacina_codigo,
+		@vacina_nome,
+		@sistema_origem
+	) SET paciente_id=@paciente_id, 
+		paciente_endereco_cep=@paciente_endereco_cep, 
+		paciente_endereco_uf=@paciente_endereco_uf,
+		paciente_endereco_nmPais=@paciente_endereco_nmPais,
+		paciente_endereco_nmMunicipio=@paciente_endereco_nmMunicipio,
+		paciente_endereco_coPais=@paciente_endereco_coPais, 
+		paciente_endereco_coIbgeMunicipio=@paciente_endereco_coIbgeMunicipio;
+        
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/VACINA.csv'
+		INTO TABLE VACINA
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@dummy,
+		@vacina_codigo,
+        @vacina_nome,
+        @vacina_fabricante_referencia
+	) SET vacina_codigo=@vacina_codigo, vacina_nome=@vacina_nome, vacina_fabricante_referencia=@vacina_fabricante_referencia;
+
+LOAD DATA LOCAL INFILE '/home/adriansl/unb/2022_2/SBD2/tf_mysql/raw_data/SISTEMA_CADASTRO.csv'
+		INTO TABLE SISTEMA_CADASTRO
+	FIELDS TERMINATED BY ','
+	ENCLOSED BY '"'
+	LINES TERMINATED BY '\n'
+	IGNORE 1 ROWS
+	(
+		@dummy,
+		@vacina_codigo,
+		@sistema
+	) SET vacina=@vacina_codigo, sistema=@sistema;
